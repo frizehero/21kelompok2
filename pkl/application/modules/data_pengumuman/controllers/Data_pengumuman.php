f8060aff5eb23bf48f3b52992ec1bbf43d8b1f85
dita544 <77608783+dita544@users.noreply.github.com>
2021-03-03 08:54:34 +0700

    Update Data_pengumuman.php

--- pkl/application/modules/data_pengumuman/controllers/Data_pengumuman.php
+++ pkl/application/modules/data_pengumuman/controllers/Data_pengumuman.php
@@ -16,8 +16,8 @@ class Data_sekolah extends MX_Controller {
 	function index()
 	{
 		$data = array(
-			'namamodule' 	=> "data_sekolah",
-			'namafileview' 	=> "V_data_sekolah",
+			'namamodule' 	=> "data_pengumuman",
+			'namafileview' 	=> "V_data_pengumuman",
 			'tampil'		=> $this->pengumuman->tampil(),
 		);
 		echo Modules::run('template/tampilCore', $data);
@@ -26,26 +26,26 @@ class Data_sekolah extends MX_Controller {
 	function tambah()
 	{
 		$this->pengumuman->tambah();
-		redirect('data_sekolah');
+		redirect('data_pengumuman');
 	}
 
 	function edit()
 	{
 		$this->pengumuman->edit();
-		redirect('data_sekolah');
+		redirect('data_pengumuman');
 	}
 
 	function hapus($id)
 	{
 		$this->pengumuman->hapus($id);
-		redirect('data_sekolah');
+		redirect('data_pengumuman');
 	}
 
 	function cari()
 	{
 		$data = array(
-			'namamodule' 	=> "data_sekolah",
-			'namafileview' 	=> "V_data_sekolah",
+			'namamodule' 	=> "data_pengumuman",
+			'namafileview' 	=> "V_data_pengumuman",
 			'tampil'		=> $this->pengumuman->cari(),
 		);
 		echo Modules::run('template/tampilCore', $data);
