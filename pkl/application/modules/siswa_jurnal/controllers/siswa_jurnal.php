297738c31aadc2914be5a47baf767632bb3fbd15
uzidreamcode <77190656+uzidreamcode@users.noreply.github.com>
2021-03-22 00:53:15 -0400

    tes template siswa

--- pkl/application/modules/siswa_jurnal/controllers/siswa_jurnal.php
+++ pkl/application/modules/siswa_jurnal/controllers/siswa_jurnal.php
@@ -26,7 +26,7 @@ class siswa_jurnal extends MX_Controller {
 			'namafileview' 	=> "V_siswa_jurnal",
 			'tampil'		=> $this->m_siswa_jurnal->tampil(),
 		);
-		echo Modules::run('template/tampilCore', $data);
+		echo Modules::run('template_siswa/tampilCore', $data);
 		$ses = $this->session->userdata('session_id');
 		echo  $ses;
 		// $data = array(
---
+++ pkl/application/modules/template_siswa/controllers/Template_siswa.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Template_siswa extends MX_Controller {
+
+	function __construct()
+	{
+		parent:: __construct();
+		//load model
+
+		$this->load->model('m_template_siswa');
+		$this->load->model('login/m_session');
+	}
+
+	
+
+	// view core templating
+	public function index()
+	{
+		
+		$this->load->view('view_template_core_siswa');
+	}
+
+	public function tampilCore($data)
+	{
+		$id = $this->session->userdata('session_id');
+		
+		$this->load->view('view_template_core_siswa',$data);
+	}
+
+}
